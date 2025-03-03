89eccf6189b7b40cb776bd8102e573ef3ae144d0
Marranzano <leomarranzano@gmail.com>
2025-03-03 10:52:03 +0000

    update

---  Operazioni matematiche/es.a.sh
+++  Operazioni matematiche/es.a.sh
@@ -4,4 +4,4 @@ echo -n "Inserisci un numero: "
 read NUM1
 echo -n "Inserisci un altro numero: "
 read NUM2
-echo "l'Addizione di num1 e num2 e' uguale a : $((NUM1 + NUM2))"
+echo "l'Addizione tra num1 e num2 e' uguale a : $((NUM1 + NUM2))"
